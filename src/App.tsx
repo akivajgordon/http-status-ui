407cbc8a4c053bb51b381271ed6bc18d16e1da2c
Akiva Gordon <akivag29@gmail.com>
2022-03-02 15:32:19 -0500

    Use hash router to deploy on GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import CreateGame from './create-game'
-import JoinGame from './join'
 import { PageWrap } from './utils'
-import Lobby from './lobby'
-import AssignRound from './assign-round'
-import AssignRoundWait from './assign-round-wait'
-import VoteRound from './vote-round'
-import VoteRoundWait from './vote-round-wait'
-import Results from './results'
 import Game from './game'
 
 function App() {
   return (
     <div style={{ margin: '3rem auto', fontSize: '1em' }}>
       <PageWrap>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<CreateGame />} />
             <Route path="/:id" element={<Game />} />
           </Routes>
-        </BrowserRouter>
-        {/* <JoinGame /> */}
-        {/* <CreateGame onGameCreated={(/> */}
-        {/* <Results /> */}
-        {/* <VoteRoundWait /> */}
-        {/* <VoteRound /> */}
-        {/* <AssignRoundWait /> */}
-        {/* <AssignRound /> */}
-        {/* <Lobby /> */}
-        {/* <Invite /> */}
+        </HashRouter>
       </PageWrap>
     </div>
   )
