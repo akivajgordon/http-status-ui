17586dcc0dc8909b6cd0d0002106852a110653e2
Akiva Gordon <akivag29@gmail.com>
2022-03-02 21:20:56 -0500

    Configure multiple environments

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_HOST = 'https://damp-coast-67022.herokuapp.com' // 'http://localhost:5001'
-export const WS_HOST = 'wss://damp-coast-67022.herokuapp.com' //'ws://localhost:5001'
+export const API_HOST = process.env.REACT_APP_API_HOST
+export const WS_HOST = process.env.REACT_APP_WS_HOST
